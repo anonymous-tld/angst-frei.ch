6c7618104dd05a9660467b2def6cc001412577a4
Joerg Roth <joerg.roth@geo.uzh.ch>
2020-09-24 17:23:54 +0200

    add parse_script

---
+++ _newposts/parsecsv.rb
@@ -0,0 +1,15 @@
+require 'csv'
+require 'date'
+
+table = CSV.parse(File.read("/Users/joroth/Downloads/export.csv"), headers: true)
+
+table.each do |n|
+  
+  d = DateTime.parse(n['pubDate'])
+  day  = d.strftime("%Y-%m-%d")
+  link =  n['Link'].dup
+  filename = day + "-Rubikon_" + n['Link'].sub!(/https:\/\/www\.rubikon\.news\/artikel\//, '') + ".md"
+  
+  File.write(filename, "---\ndate:       " + day + "\nredirect:   " + link + "\ntitle:      Rubikon\nsubtitle:   \"" + n['Title'] + "\"\ncategories: Medien\ntags:       [rubikon]\n---\n")
+
+end
