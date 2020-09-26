d82531e4cda2df3aab9a75e0a0928199a4404b91
Joerg Roth <info@leylines.ch>
2020-09-26 14:46:46 +0200

    add de-locale

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     },
     firstDay: 1,
+    locale: 'de',
     themeSystem: 'bootstrap',
     initialView: 'listWeek',
     dayMaxEvents: 5,
