9e8f6a78e638c46469fc4f0c616d35e586097692
Joerg Roth <joerg.roth@geo.uzh.ch>
2020-09-28 17:31:08 +0200

    better calender

---
+++ _posts/2020-09-26-bitchute_xD5gvDL3RIGb.md
@@ -0,0 +1,11 @@
+---
+layout:     post
+title:      bitchute
+subtitle:   "Wichtige Botschaft von Neurologin Dr. Margareta Griesz-Brisson"
+date:       2020-09-26
+categories: Video
+tags:       [bitchute, maske]
+bitchuteId: "xD5gvDL3RIGb"
+---
+
+{% include bitchutePlayer.html id=page.bitchuteId %}
--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,9 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
     headerToolbar: {
-      left: 'dayGridMonth,timeGridDay',
-      center: 'title',
-      right: 'prev,next'
+      start: 'title', // will normally be on the left. if RTL, will be on the right
+      center: '',
+      end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
+    },
+    footerToolbar: {
+      left: 'listWeek,dayGridMonth,timeGridDay',
+      right: 'addButton'
+    },
+    customButtons: {
+      addButton: {
+        text: 'Neuer Termin',
+        click: function() {
+          window.open("https://demo.terminkalender.top/add/");
+        }
+      }
     },
     events:'/event-data',
     eventClick: function(info) {
@@ -13,15 +25,19 @@ document.addEventListener('DOMContentLoaded', function() {
         window.open(info.event.url);
       }
     },
-    firstDay: 1,
+    navLinks: true,
     locale: 'de',
     themeSystem: 'bootstrap',
     initialView: 'listWeek',
     dayMaxEvents: 5,
+    firstDay: 1,
     eventTimeFormat: {
       hour: '2-digit',
       minute: '2-digit',
       hour12: false
+    },
+    eventClassNames: function(arg) {
+      return [ arg.event.extendedProps.country.toLowerCase() + "-flag" ]
     }
   });
   calendar.render();
