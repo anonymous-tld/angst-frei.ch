a73bdafdcd7acc0d952250e4460e6e001e024c33
Joerg Roth <joerg.roth@geo.uzh.ch>
2020-10-07 09:36:44 +0200

    update geocode

--- geocode/bin/geocode.py
+++ geocode/bin/geocode.py
@@ -8,13 +8,27 @@ locator = Nominatim(user_agent="angst-frei")
 df = pd.read_csv("../../_data/demotermine.csv")
 df.head()
 
-df["geocode_col"] = df["treffpunkt"].astype(str) + ',' + \
+df["gc_col_full"] = df["treffpunkt"].astype(str) + ',' + \
                     df["postleitzahl"].astype(str) + ',' + \
                     df["stadt"] + ',' + \
                     df["land"]   
 
+df["gc_col_short"] = df["postleitzahl"].astype(str) + ',' + \
+                     df["stadt"] + ',' + \
+                     df["land"]   
+
+df["gc_col_plz"] = df["postleitzahl"].astype(str) + ',' + \
+                     df["land"]   
+
 geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
-df["location"] = df["geocode_col"].apply(geocode)
+df["location"] = df["gc_col_full"].apply(geocode)
+if df["location"] is None:
+  df["location"] = df["gc_col_short"].apply(geocode)
+if df["location"] is None:
+  df["location"] = df["gc_col_plz"].apply(geocode)
+
+print(df["location"])
+
 df["point"] = df["location"].apply(lambda loc: tuple(loc.point) if loc else None)
 df[["latitude", "longitude", "altitude"]] = pd.DataFrame(df["point"].tolist(), index=df.index)
 
