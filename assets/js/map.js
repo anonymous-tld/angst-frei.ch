6e66b5cdbc1f9facf874630962ff99804002b1df
Joerg Roth <joerg.roth@geo.uzh.ch>
2020-10-09 10:23:28 +0200

    change termine to better dir

--- assets/js/map.js
+++ assets/js/map.js
@@ -28,7 +28,7 @@ controlLayers.addBaseLayer(terrain, 'Stamen Terrain basemap');
 geocoder = new L.Control.Geocoder.Nominatim();
 
 // Read markers data from data.csv
-$.get('/demotermine_geo.csv', function(csvString) {
+$.get('/assets/data/demotermine_geo.csv', function(csvString) {
 
   // Use PapaParse to convert string to array of objects
   var data = Papa.parse(csvString, {header: true, dynamicTyping: true, skipEmptyLines: true}).data;
